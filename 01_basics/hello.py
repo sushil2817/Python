c640b92c55ca3fcfadbf465741ec3cd203f968c9
sushil2817 <tony281723@gmail.com>
2024-05-30 11:42:15 +0530

    learning about python shell, mutable and immutable variable

--- 01_basics/hello.py
+++ 01_basics/hello.py
@@ -1,7 +1,11 @@
-print("hello world")
+# python has garbag collector
+print("hello world!")
 
 
 def chai(n):
     print(n)
 
 chai("hello sushil")
+chai_one = "lemon tea"
+chai_two = "ginger tea"
+chai_three = "masala chai"
